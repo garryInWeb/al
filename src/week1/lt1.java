d5e5364ab3c55d31d1804dbec7d85d672349de60
zheng <987357718@qq.com>
2021-01-11 10:23:06 +0800

    t1

---
+++ src/week1/lt1.java
@@ -0,0 +1,46 @@
+package week1;
+
+public class lt1 {
+    /**
+     * Definition for singly-linked list.
+     **/
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+
+    public static void main(String[] args) {
+
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int temp = 0;
+        ListNode resHead = new ListNode();
+        ListNode pre = resHead;
+        do{
+            ListNode cur = new ListNode(0);
+            if(l1 != null){
+                cur.val = cur.val + l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                cur.val = cur.val + l2.val;
+                l2 = l2.next;
+            }
+            cur.val = cur.val + temp;
+            temp = cur.val / 10;
+            cur.val = cur.val % 10;
+            pre.next = cur;
+        } while(l1 != null || l2 != null);
+        // !! 别忘了最后一位
+        if(temp > 0){
+            ListNode cur = new ListNode(1);
+            pre.next = cur;
+        }
+
+        return resHead.next;
+    }
+}
